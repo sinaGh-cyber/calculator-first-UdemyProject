c68e754b2247fa8b37eec34b7efd03edaa350920
daitenma <sinaina1377@gmail.com>
2021-04-26 23:31:42 +0430

    26/04/2021 23:31

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,5 @@
 let cuurentResult = 0;
+const p = 3.14;
 cuurentResult = cuurentResult + 10;
-outputResult(cuurentResult, 'formola');
+let formola = `the formola is equle to = ${cuurentResult}`;
+outputResult(cuurentResult, formola);
