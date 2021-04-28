a4b37748891323a6d6f596451e809617e28c6ee2
daitenma <sinaina1377@gmail.com>
2021-04-29 01:51:31 +0430

    29/04/2021 01:51 how to work eith functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,7 +3,7 @@ let cuurentResult = 0;
 cuurentResult = cuurentResult + 10;
 let formola = `the formola is equle to = ${cuurentResult}`;
 function add (num1, num2) {
-    const result = cuurentResult + userInput.value;
-    outputResult(result, ` `);
+    cuurentResult = cuurentResult + userInput.value;
+    outputResult(cuurentResult, ` `);
 }
 addBtn.addEventListener(`click`, add);
