d6cdf59bf589819a51c8a3698b3d59146017b558
daitenma <sinaina1377@gmail.com>
2021-04-29 14:52:26 +0430

    29/04/2021 14:52 simple calculator

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,39 @@
-const defaultResult = 0;
-let currentResult = defaultResult;
+let currentResult = 0;
 
-function add() {
-  const calcDescription = `${currentResult} + ${userInput.value}`;
-  currentResult = currentResult + parseInt(userInput.value);
+function getUserNumber(){
+  const number = parseInt(userInput.value);
+  return number;
+}
+function creatAndWriteOutput(opreator, resultBeforCalc, calcNumber) {
+  const calcDescription = `${resultBeforCalc}  ${opreator} ${calcNumber}`;
   outputResult(currentResult, calcDescription);
 }
+function add() {
+  initializResult = currentResult;
+  enterdNumber = getUserNumber();
+  currentResult = initializResult + enterdNumber;
+  creatAndWriteOutput('+', initializResult, enterdNumber )
+  
+}
+function subtrac() {
+  initializResult = currentResult;
+  enterdNumber = getUserNumber();
+  currentResult = initializResult - enterdNumber;
+  creatAndWriteOutput('-', initializResult, enterdNumber )
+}
+function multiply() {
+  initializResult = currentResult;
+  enterdNumber = getUserNumber();
+  currentResult = initializResult * enterdNumber;
+  creatAndWriteOutput('*', initializResult, enterdNumber )
+}
+function divide() {
+  initializResult = currentResult;
+  enterdNumber = getUserNumber();
+  currentResult = initializResult / enterdNumber;
+  creatAndWriteOutput('/', initializResult, enterdNumber )
+}
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtrac);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide)
